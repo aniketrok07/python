11d386e639129e4df3bd75043d0b111e767d3ed8
aniketrok07 <aniketrokade10@gmail.com>
2023-07-01 22:59:59 +0530

    Added userInput code

---
+++ userInput.py
@@ -0,0 +1,11 @@
+# a = input()
+# b = input("Enter your age")
+
+# print("My name is ",a)
+# print("my age is ",b)
+
+x = input(" Enter the number x:")
+y = input("Enter the number y:")
+
+print(x + y)
+print(int(x) + int(y))
